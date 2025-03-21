eeb098190e61089badb31a5b6494ad0e86019036
likelasttime <46569105+likelasttime@users.noreply.github.com>
2025-03-21 11:35:15 +0900

    Title: 아름다운 수, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250321/아름다운 수/beautiful-number.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    static int answer;      // N자리 아름다운 수의 갯수
+    static int n;
+    static int[] arr;
+
+    public static void dfs(int depth) {
+        if(depth >= n) {
+            int[] cntArr = new int[5];
+            int prev = arr[0];
+            for(int i=0; i<n; i++) {
+                if(arr[i] == prev) {
+                    cntArr[arr[i]]++;
+                } else {
+                    cntArr[arr[i]] = 1;
+                }
+                prev = arr[i];      // 이전 값 갱신
+            }
+            for(int i=1; i<=4; i++) {
+                if(cntArr[i] % i != 0) {
+                    return;
+                }
+            }
+            answer++;
+            return;
+        }
+        for(int i=1; i<=4; i++) {
+            arr[depth] = i;
+            dfs(depth + 1);
+            arr[depth] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        arr = new int[n];
+
+        dfs(0);
+        System.out.println(answer);
+    }
+}
