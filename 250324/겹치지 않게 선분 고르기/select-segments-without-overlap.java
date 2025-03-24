4886b0e5df03fd17c28395a08a67cc609c1b410b
likelasttime <46569105+likelasttime@users.noreply.github.com>
2025-03-24 16:51:29 +0900

    Title: 겹치지 않게 선분 고르기, Time: 3068ms, Memory: 14MB, Status: Time Limit Exceed - CodeTree

--- 250324/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 250324/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
     static int answer;
     static int n;       // 1 <= 선분의 개수 <= 15
-    static boolean[] visit;     // 선택한 선분 표시
+    static List<Integer> line;    // 선택한 선분 저장
     static int[][] segments;
 
     public static boolean isOverlapped(int cur) {
         int curLeft = segments[cur][0];
         int curRight = segments[cur][1];
-        for(int idx=0; idx<n; idx++) {
-            if(!visit[idx]) {       // 선택하지 않은 선분이라면
-                continue;
-            } else if(idx == cur) {     // 자기 자신 건너뛰기
-                continue;
-            }
+        for(int idx : line) {
             int left = segments[idx][0];
             int right = segments[idx][1];
             // 양끝점이 겹치는 경우
@@ -37,21 +34,15 @@ public class Main {
         선분을 선택하는 조합을 바꿔가며 재귀호출
     */
     public static void dfs(int depth, int cnt) {
-        if(depth == n) {        // 모든 선분을 골랐으면
-            answer = Math.max(answer, cnt);
-            return;
-        }
+        answer = Math.max(answer, cnt);
         for(int i=depth; i<n; i++) {       // i = 선분 인덱스
-            if(!visit[i]) {     // 아직 선택하지 않은 선분이라면
-                boolean result = isOverlapped(i);
-                int cntLine = 0;
-                if(!result) {       // 선분을 그릴 수 있으면
-                    cntLine = 1;
-                    visit[i] = true;
-                }
-                dfs(depth + 1, cnt + cntLine);
-                visit[i] = false;
+            boolean result = isOverlapped(i);
+            if(result) {        // 선분이 겹치면
+                continue;
             }
+            line.add(i);
+            dfs(depth + 1, cnt + 1);
+            line.remove(depth);
         }
     }
 
@@ -60,7 +51,7 @@ public class Main {
         n = sc.nextInt();
         segments = new int[n][2];
         answer = 0;
-        visit = new boolean[n];
+        line = new ArrayList();
         // 선분의 양끝점 좌표 입력받기
         for (int i = 0; i < n; i++) {
             segments[i][0] = sc.nextInt();      // 1 ~ 1,000
